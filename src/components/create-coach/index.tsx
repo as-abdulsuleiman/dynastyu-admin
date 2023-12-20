3529c1a5b7cdc460ecf14120661da116a7e1366e
Abdul Suleiman <a.s.abdulsuleiman@gmail.com>
2023-12-20 11:09:54 +0530

    fix: create coach modal

--- src/components/create-coach/index.tsx
+++ src/components/create-coach/index.tsx
@@ -168,7 +168,7 @@ const CreateCoach: FC<CreateCoachProps> = ({ isOpen, onClose, onRefetch }) => {
       <DialogTrigger asChild>
         <Button>Add New Coach</Button>
       </DialogTrigger>
-      <DialogContent className="container mx-auto max-w-2xl">
+      <DialogContent className="container mx-auto max-w-3xl max-h-full overflow-y-auto px-[16px] md:px-[2rem] py-[2rem] sheet-scroll">
         <DialogHeader>
           <DialogTitle className="text-center text-3xl">Add Coach</DialogTitle>
         </DialogHeader>
