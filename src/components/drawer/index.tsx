332be9213a1e46340d3bd4decc022b76aa2e707b
Abdul Suleiman <a.s.abdulsuleiman@gmail.com>
2024-02-03 01:31:01 +0530

    feat: sidebar items

--- src/components/drawer/index.tsx
+++ src/components/drawer/index.tsx
@@ -18,16 +18,11 @@ import { useAuth } from "@/hooks/useAuth";
 import { useRootStore } from "@/mobx";
 import { observer } from "mobx-react-lite";
 import Image from "next/image";
+import SidebarItems from "../sidebar-items";
 
 interface DrawerProps {}
 
-type IconProps = {
-  className: string;
-  color: string;
-};
-
 const Drawer: FC<DrawerProps> = () => {
-  const pathname = usePathname();
   const { logout } = useAuth();
   const router = useRouter();
 
@@ -37,64 +32,6 @@ const Drawer: FC<DrawerProps> = () => {
 
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
-  const items = [
-    {
-      name: "Dashboard",
-      path: "/dashboard",
-      icon: ({ className, color }: IconProps) => (
-        <Home className={className} color={color} />
-      ),
-      hasFill: false,
-    },
-    // {
-    //   name: "Users",
-    //   hasFill: false,
-    //   path: "/users",
-    //   icon: ({ className, color }: IconProps) => (
-    //     <Users className={className} color={color} />
-    //   ),
-    // },
-    // {
-    //   name: "Athletes",
-    //   hasFill: false,
-    //   path: "/athletes",
-    //   icon: ({ className, color }: IconProps) => (
-    //     <Medal className={className} color={color} />
-    //   ),
-    // },
-    {
-      name: "Coaches",
-      hasFill: false,
-      path: "/coaches",
-      icon: ({ className, color }: IconProps) => (
-        <Grid2X2 className={className} color={color} />
-      ),
-    },
-    // {
-    //   name: "Fans",
-    //   hasFill: false,
-    //   path: "/fans",
-    //   icon: ({ className, color }: IconProps) => (
-    //     <Medal className={className} color={color} />
-    //   ),
-    // },
-    // {
-    //   name: "Schools",
-    //   hasFill: false,
-    //   path: "/schools",
-    //   icon: ({ className, color }: IconProps) => (
-    //     <School className={className} color={color} />
-    //   ),
-    // },
-    {
-      name: "Settings",
-      hasFill: false,
-      path: "/settings",
-      icon: ({ className, color }: IconProps) => (
-        <Settings className={className} color={color} />
-      ),
-    },
-  ];
   return (
     <Sheet open={isOpen} onOpenChange={() => setIsOpen(!isOpen)}>
       <SheetTrigger asChild>
@@ -119,44 +56,12 @@ const Drawer: FC<DrawerProps> = () => {
         </SheetHeader>
         <div className="w-full pb-8 h-full ml-[-8px]">
           <div className="relative w-full h-full pt-[55px]">
-            {items?.map((val, index) => {
-              const isActive = pathname === val.path;
-              const iconClass = `h-3 w-3 sm:h-4 sm:w-4 ${
-                val?.hasFill
-                  ? "group-hover:fill-[#2BD984] text-[#717070] group-hover:text-[#2BD984] fill-[#717070]"
-                  : "group-hover:stroke-[#2BD984] text-[#717070] group-hover:text-[#2BD984] stroke-[#717070]"
-              } ${
-                isActive
-                  ? `${val.hasFill ? "fill-[#2BD984]" : "stroke-[#2BD984]"}`
-                  : `${val.hasFill ? "fill-[#D8D3CB]" : "stroke-[#D8D3CB]"}`
-              }`;
-              const Icon = () => val?.icon({ className: iconClass, color: "" });
-              return (
-                <div
-                  key={index}
-                  className="mt-4"
-                  onClick={() => {
-                    router.push(val?.path, { scroll: true });
-                    setIsOpen(!isOpen);
-                  }}
-                >
-                  <div
-                    className={`group ${
-                      isActive ? "bg-[#dc2626]" : "bg-transparent"
-                    }  flex cursor-pointer items-center border rounded-md py-[6px] px-[16px]`}
-                  >
-                    <Icon />
-                    <div
-                      className={`${
-                        isActive ? "text-[#e5e7eb]" : ""
-                      } ml-3 mt-[0px] my-0`}
-                    >
-                      {val?.name}
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
+            <SidebarItems
+              handleNavigation={(val) => {
+                router.push(val);
+                setIsOpen(false);
+              }}
+            />
             <div className="absolute bottom-[8px] w-full flex  justify-start items-start">
               <div className="flex w-full flex-col">
                 <Button
--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -3,191 +3,20 @@
 "use client";
 
 import { FC } from "react";
-import { FolderRoot, Home, Settings } from "lucide-react";
-import { usePathname, useParams, useRouter } from "next/navigation";
 import ThemeToggle from "../theme-toggle";
 import { observer } from "mobx-react-lite";
-import { Icons } from "../Icons";
-import Link from "next/link";
-import { Text } from "@tremor/react";
+import SidebarItems from "../sidebar-items";
+import { useRouter } from "next/navigation";
 
 interface SideBarProps {}
-type IconProps = {
-  className: string;
-  color: string;
-};
 
 const SideBar: FC<SideBarProps> = ({}) => {
-  const pathname = usePathname();
   const router = useRouter();
-
-  const items = [
-    {
-      name: "Dashboard",
-      path: "/dashboard",
-      icon: ({ className, color }: IconProps) => (
-        <Home className={className} color={color} />
-      ),
-      hasFill: false,
-      items: [],
-    },
-    // {
-    //   name: "Users",
-    //   hasFill: false,
-    //   path: "/users",
-    //   icon: ({ className, color }: IconProps) => (
-    //     <Users className={className} color={color} />
-    //   ),
-    // },
-    {
-      name: "Athletes",
-      hasFill: false,
-      path: "/athletes",
-      items: [],
-      icon: ({ className, color }: IconProps) => (
-        <Icons.athlete className={className} color={color} />
-      ),
-    },
-    {
-      name: "Coaches",
-      hasFill: true,
-      path: "/coaches",
-      items: [
-        {
-          name: "Create Coach",
-          path: "/coaches/new",
-        },
-      ],
-      icon: ({ className, color }: IconProps) => (
-        <Icons.whistle className={className} color={color} />
-      ),
-    },
-    // {
-    //   name: "Fans",
-    //   hasFill: false,
-    //   path: "/fans",
-    //   icon: ({ className, color }: IconProps) => (
-    //     <Medal className={className} color={color} />
-    //   ),
-    // },
-    {
-      name: "Schools",
-      hasFill: false,
-      path: "/schools",
-      items: [
-        {
-          name: "Create School",
-          path: "/schools/new",
-        },
-      ],
-      icon: ({ className, color }: IconProps) => (
-        <Icons.school className={className} color={color} />
-      ),
-    },
-    {
-      name: "Skills",
-      hasFill: false,
-      path: "/skills",
-      items: [
-        // {
-        //   name: "Create Skill",
-        //   path: "/skills/new",
-        // },
-        {
-          name: "Verification Request",
-          path: "/skills/verification-request",
-        },
-      ],
-      icon: ({ className, color }: IconProps) => (
-        <FolderRoot className={className} color={color} />
-      ),
-    },
-    {
-      name: "Settings",
-      hasFill: false,
-      path: "/settings",
-      items: [],
-      icon: ({ className, color }: IconProps) => (
-        <Settings className={className} color={color} />
-      ),
-    },
-  ];
   return (
     <div className="w-full px-6 pt-6 pb-8 h-full shadow-xl">
       <div className="relative w-full h-full">
         <div className="pt-[60px]">
-          {items?.map((val, index) => {
-            // const isActive = pathname.includes(val?.path);
-
-            const parentPath = val?.path;
-            let pathToCheck: any = parentPath?.split("?");
-            pathToCheck = pathToCheck?.length ? pathToCheck[0] : null;
-            const childPath = val?.items?.some((data: any) =>
-              pathname.includes(data?.path)
-            );
-            const isActive = pathname.includes(pathToCheck) || childPath;
-
-            const iconColor = "";
-            const iconClass = `h-[19px] w-[19px] ${
-              val?.hasFill
-                ? "group-hover:fill-teal-600  text-tremor-default fill-tremor-content-emphasis dark:fill-dark-tremor-content-emphasis "
-                : "group-hover:stroke-teal-600 text-tremor-default stroke-tremor-content-emphasis dark:stroke-dark-tremor-content-emphasis"
-            } ${
-              isActive &&
-              `${
-                val?.hasFill
-                  ? "fill-tremor-content-teal dark:fill-dark-tremor-content-teal"
-                  : "stroke-tremor-content-teal dark:stroke-dark-tremor-content-teal"
-              }`
-            }`;
-            const Icon = () =>
-              val?.icon({ className: iconClass, color: iconColor });
-            return (
-              <div key={index}>
-                <Link
-                  href={val.path}
-                  shallow={false}
-                  scroll={true}
-                  prefetch={true}
-                >
-                  <div
-                    className={`group ${
-                      isActive
-                        ? "bg-secondary"
-                        : "bg-background dark:bg-dark-background"
-                    } "bg-background dark:bg-dark-background flex cursor-pointer items-center py-[6px] px-[16px] border rounded-sm mt-4 hover:scale-105 transition-transform ease-out duration-200`}
-                  >
-                    <Icon />
-                    <Text
-                      className={`${
-                        isActive
-                          ? "text-tremor-content-teal dark:text-dark-tremor-content-teal"
-                          : "text-tremor-content-emphasis dark:text-dark-tremor-content-emphasis group-hover:dark:text-dark-tremor-content-teal group-hover:text-tremor-content-teal"
-                      } ml-4 mt-[0px] text-3xl`}
-                    >
-                      {val?.name}
-                    </Text>
-                  </div>
-                </Link>
-                {isActive && val?.items?.length ? (
-                  <div className="px-2 pb-3">
-                    {val?.items.map((val, index) => {
-                      return (
-                        <div key={index}>
-                          <div
-                            className="cursor-pointer text-[14px] mt-2"
-                            onClick={() => router.push(val?.path)}
-                          >
-                            {val?.name}
-                          </div>
-                        </div>
-                      );
-                    })}
-                  </div>
-                ) : null}
-              </div>
-            );
-          })}
+          <SidebarItems handleNavigation={(val) => router.push(val)} />
         </div>
         <div className="absolute bottom-0">
           <ThemeToggle />
